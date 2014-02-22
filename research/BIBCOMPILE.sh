cf1cd40edc5e60e8aa7f57d65f31ae611d5ae7a9
wlandau <will.landau@gmail.com>
2014-02-22 00:32:12 -0600

    cv

--- research/BIBCOMPILE.sh
+++ research/BIBCOMPILE.sh
@@ -4,6 +4,6 @@ pdflatex publications.tex
 bibtex publications
 pdflatex publications.tex
 
-cp publications.bbl ../cv/cv.bbl
-cd ../cv
-pdflatex cv.tex
+# cp publications.bbl ../cv/cv.bbl
+# cd ../cv
+# pdflatex cv.tex
